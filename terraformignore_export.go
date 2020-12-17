d7a819a0d37d8905d654de5aa110673fa06db233
Louis Garman <louisgarman@gmail.com>
2020-12-17 07:08:50 +0000

    Export rule type

--- terraformignore_export.go
+++ terraformignore_export.go
@@ -2,6 +2,15 @@ package slug
 
 import "io"
 
+// Rule exports unexported rule
+type Rule struct {
+	rule
+}
+
+func (r *Rule) Match(path string) (bool, error) {
+	return r.rule.match(path)
+}
+
 // ParseIgnoreFile exports unexported parseIgnoreFile
 func ParseIgnoreFile(rootPath string) []rule {
 	return parseIgnoreFile(rootPath)

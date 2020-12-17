a61d87bcd64a1a71892aba5f29dad7d187c99dbb
Louis Garman <louisgarman@gmail.com>
2020-12-17 07:00:46 +0000

    Export more funcs/vars

--- terraformignore_export.go
+++ terraformignore_export.go
@@ -1,5 +1,7 @@
 package slug
 
+import "io"
+
 // ParseIgnoreFile exports unexported parseIgnoreFile
 func ParseIgnoreFile(rootPath string) []rule {
 	return parseIgnoreFile(rootPath)
@@ -9,3 +11,11 @@ func ParseIgnoreFile(rootPath string) []rule {
 func MatchIgnoreRule(path string, rules []rule) bool {
 	return matchIgnoreRule(path, rules)
 }
+
+// ReadRules exports unexported readRules
+func ReadRules(input io.Reader) []rule {
+	return readRules(input)
+}
+
+// DefaultExclusions exports unexported defaultExclusions
+var DefaultExclusions = defaultExclusions

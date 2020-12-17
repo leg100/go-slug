a7f901779b72ca8305414108cb712f9bdf7ccec8
Louis Garman <louisgarman@gmail.com>
2020-12-17 06:40:56 +0000

    Export matchIgnoreRule

--- terraformignore_export.go
+++ terraformignore_export.go
@@ -4,3 +4,8 @@ package slug
 func ParseIgnoreFile(rootPath string) []rule {
 	return parseIgnoreFile(rootPath)
 }
+
+// MatchIgnoreRule exports unexported matchIgnoreRule
+func MatchIgnoreRule(path string, rules []rule) bool {
+	return matchIgnoreRule(path, rules)
+}
